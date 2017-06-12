6a1c89dc948ceb5e28001df261af93bef4942009
Gocchi <centenoilse93@gmail.com>
2017-06-12 19:47:45 +0200

    Obtuve data name de cada pokemon

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,22 @@
-console.log("Bienvenido");
+var cargarPagina= function(){
+	cargarPokemons();
+}
+var cargarPokemons=function(){
+	var url= "http://pokeapi.co/api/v2/pokemon-species";
+	$.getJSON(url, function(response){
+		var pokemones= response.results;
+		mostrarPokemones(pokemones);
+	});
+}
+
+var mostrarPokemones= function(pokemones){
+	pokemones.forEach(function(pokemones){
+		var $ul= $("#pokemones");
+		var $li =$("<li />");
+
+		$ul.append($li);
+		$li.text(pokemones.name);
+
+	});
+}
+$(document).ready(cargarPagina);
